379e6dd26315d67ff80b38be0e828d96d45e685e
EC2 Default User <ec2-user@ip-172-31-11-52.us-west-2.compute.internal>
2022-04-05 16:09:15 +0000

    Update helper script

--- create_environment.sh
+++ create_environment.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo -e "\n========================================\n\nApplication Configuration\n\n========================================"
 eb init
+echo -e "\n========================================\n\nEnvironment Configuration\n\n========================================"
 eb create -i $1 --scale $2 --instance_profile aws-elasticbeanstalk-locust-role
--- terminate_environment.sh
+++ terminate_environment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+echo -e "\n========================================\n\nCleaning up resources\n\n========================================"
 eb terminate --all

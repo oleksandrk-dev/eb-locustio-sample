269fc6f0f1f20ce528fc3712d1a0fc34073b35c3
Oleksandr Kuzhel <oleksandrk@remitly.com>
2023-01-20 11:07:32 +0100

    Changed Procfile to work on new Amazon isntances

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-locust-master: /bin/bash -c "exec /usr/local/bin/locust -f locustfile.py --master"
-locust-follower: /bin/bash -c "exec /usr/local/bin/locust -f locustfile.py --worker --master-host=$(<.masterIP)"
+locust-master: exec /usr/local/bin/locust -f locustfile.py --master
+locust-follower: exec /usr/local/bin/locust -f locustfile.py --worker --master-host=$(<.masterIP)
--- locustfile.py
+++ locustfile.py
@@ -1,19 +1,8 @@
-import math
-from collections import namedtuple
-
-from locust import HttpUser, task, constant_throughput, TaskSet, constant, LoadTestShape, constant_pacing
+from locust import task, TaskSet
 
 
-# preprod: https://preprod.dev.remitly.com
 class UserTasks(TaskSet):
     @task
     def get_url(self):
-        response = self.client.get("/us/en/india")
+        self.client.get("/us/en/india")
         self.client.cookies.clear()
-
-
-class WebsiteUser(HttpUser):
-    wait_time = constant_throughput(50)
-    # stop_timeout = 20
-    tasks = [UserTasks]
-

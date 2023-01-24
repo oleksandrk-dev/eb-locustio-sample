4668bd9ea076f4f04c6163df2ca3cb0bad327fdb
Oleksandr Kuzhel <oleksandrk@remitly.com>
2023-01-24 13:04:35 +0100

    Set the landing pages

--- locustfile.py
+++ locustfile.py
@@ -4,6 +4,18 @@ from locust import HttpUser, task, constant_throughput
 class QuickstartUser(HttpUser):
     wait_time = constant_throughput(1)
 
-    @task
-    def landing_page(self):
+    @task(3)
+    def landing_page1(self):
         self.client.get("/us/en/philippines")
+
+    @task(3)
+    def landing_page2(self):
+        self.client.get("/us/es/méxico")
+
+    @task(3)
+    def landing_page3(self):
+        self.client.get("/ca/en/india")
+
+    @task(3)
+    def landing_page4(self):
+        self.client.get("/us/en/pakistan")
